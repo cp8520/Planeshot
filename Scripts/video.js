fa49ae09af8b9c9d780fe35b7e598f8b7d83de14
dankjank91 <cp85208520@gmail.com>
2025-10-31 00:44:09 -0600

    Update

--- Scripts/video.js
+++ Scripts/video.js
@@ -1,6 +1,8 @@
 const videoLinks = [
   'https://youtu.be/T81BAzcU7Bw',
   'https://youtu.be/w5pHiqktdvA',
+  'https://assets.churchofjesuschrist.org/c7d7c1a695a111ecb9e7eeeeac1e43a96b7575bc-1080p-en.mp4',
+  
 ];
 
 const videoFrame = document.getElementById('videoFrame');
